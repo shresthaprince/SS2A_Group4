3fcf3194259d50d95c66ca1fbaaf88dcdd4d46dc
Rico-sudo <albericosss@hotmail.it>
2020-07-29 11:19:32 +1000

    Support page and forgot page base and for done

--- study-groups-frontend/src/components/forgotpsw.jsx
+++ study-groups-frontend/src/components/forgotpsw.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import "../styles/login.css";
 
 class Forgotpsw extends Component {
 render()
-{return(<div>
-    <h5>Forgot psw page</h5>
+{return(<div class="form-group">
+    <p><h3>Reset Your Password</h3>
+Enter the email address you used when creating the account and click Send Password Reset Email.
+<p> A message will be sent to that address containing a link to reset your password.</p>
+
+If you do not receive your email within five minutes check your spam folder.
+
+</p>
+    <form action="" class="forgot-form">
+        <label for="email">Insert your E-mail </label>
+        <input type="email" name="email" id="inputEmail" className="form-control"></input>
+        
+        <div class="mt-2 col-md-12"> </div>
+        <button className="btn btn-primary btn-block" type="submit"> Send reset Link</button>
+        
+ 
+    </form>
 </div>)}}
 export default Forgotpsw;
--- study-groups-frontend/src/components/support.jsx
+++ study-groups-frontend/src/components/support.jsx
@@ -3,6 +3,20 @@ import React, { Component } from "react";
 class Support extends Component {
 render()
 {return(<div>
-    <h5>Support page</h5>
+    <h4>Support page</h4>
+    
+    <form action="" class="forgot-form">
+        <label for="email">Insert your E-mail </label>
+        <input type="email" name="email" id="inputEmail" className="form-control"></input>
+        <label for="text">Subject </label>
+        <input type="text" name="subject" id="subject" className="form-control"></input>
+        <label for="text">Description of the problem</label>
+        <input type="text" name="description" id="description" className="form-control"></input>
+        
+        <div class="mt-2 col-md-12"> </div>
+        <button className="btn btn-primary btn-block" type="submit"> Send</button>
+        
+ 
+    </form>
 </div>)}}
 export default Support;
