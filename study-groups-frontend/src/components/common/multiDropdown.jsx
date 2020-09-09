a9358b6f1bb7ddc5468e495f52b8792077ef12ad
Rico-sudo <albericosss@hotmail.it>
2020-09-10 00:28:30 +1000

    m component

--- study-groups-frontend/src/components/common/multiDropdown.jsx
+++ study-groups-frontend/src/components/common/multiDropdown.jsx
@@ -1,15 +1,20 @@
 import React , {useState} from 'react';
 import {Multiselect} from 'multiselect-react-dropdown';
+import { getStudents } from "../../services/studentService";
+import { trackPromise } from "react-promise-tracker";
+
 
 
 function Dropdown(){
-    const data = [
+    
+    const {data} = [
       {student: '1' , id: 1},
       {student: '2' , id: 2},
       {student: '3' , id: 3}
     ]
+   // const { data: students } = await trackPromise(getStudent());
   const [options] = useState(data);
-  const [dropdownCount, setDropdownCount] = React.useState(0);
+  const [dropdownCount, setDropdownCount] = React.useState(1);
   return (
     <div>
       {Array.from({ length: dropdownCount }).map((v, idx) => {
